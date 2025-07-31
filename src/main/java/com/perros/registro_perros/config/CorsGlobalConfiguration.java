4c822e7f0ea2f9c341e5c438ef8664d178344c4d
JuanDiego <juandiegoflores0202@gmail.com>
2025-07-31 02:22:41 -0500

    Update CorsGlobalConfiguration.java

--- src/main/java/com/perros/registro_perros/config/CorsGlobalConfiguration.java
+++ src/main/java/com/perros/registro_perros/config/CorsGlobalConfiguration.java
@@ -2,26 +2,27 @@ package com.perros.registro_perros.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
 public class CorsGlobalConfiguration {
 
     @Bean
-    public WebFluxConfigurer corsConfigurer(){
+    public WebFluxConfigurer corsConfigurer() {
         return new WebFluxConfigurer() {
             @Override
-            public void addCorsMappings(org.springframework.web.reactive.config.CorsRegistry registry) {
+            public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://localhost:5173", "http://localhost:4173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                        .allowedOrigins(
+                            "http://localhost:3000",
+                            "http://localhost:5173",
+                            "http://localhost:4173"
+                        )
+                        .allowedMethods("*")
                         .allowedHeaders("*")
-                        .exposedHeaders("Authorization")
-                        .allowCredentials(true)
-                        .maxAge(3600);
-            }            
-            
+                        .allowCredentials(true);
+            }
         };
     }
-    
 }

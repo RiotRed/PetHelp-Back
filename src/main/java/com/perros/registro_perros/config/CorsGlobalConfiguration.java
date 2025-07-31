07656acd90fdc427b4550fd9e761b589ec42325b
JuanDiego <juandiegoflores0202@gmail.com>
2025-07-31 02:32:40 -0500

    Update CorsGlobalConfiguration.java

--- src/main/java/com/perros/registro_perros/config/CorsGlobalConfiguration.java
+++ src/main/java/com/perros/registro_perros/config/CorsGlobalConfiguration.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.server.WebFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class CorsGlobalConfiguration {
@@ -23,6 +29,26 @@ public class CorsGlobalConfiguration {
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
+        };
+    }
+
+    // Filtro CORS global para controladores y routers funcionales
+    @Bean
+    public WebFilter corsFilter() {
+        return (ServerWebExchange ctx, org.springframework.web.server.WebFilterChain chain) -> {
+            ServerHttpRequest request = ctx.getRequest();
+            String origin = request.getHeaders().getOrigin();
+            if (origin != null) {
+                ctx.getResponse().getHeaders().add("Access-Control-Allow-Origin", origin);
+                ctx.getResponse().getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+                ctx.getResponse().getHeaders().add("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization");
+                ctx.getResponse().getHeaders().add("Access-Control-Allow-Credentials", "true");
+            }
+            if (request.getMethod() == HttpMethod.OPTIONS) {
+                ctx.getResponse().setStatusCode(org.springframework.http.HttpStatus.OK);
+                return Mono.empty();
+            }
+            return chain.filter(ctx);
         };
     }
 }

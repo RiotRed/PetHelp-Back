355856517c9903264ac266553dab6352a75ab541
Piero Rojas <133281510+RiotRed@users.noreply.github.com>
2025-07-31 10:47:22 -0500

    Revert "Update CorsGlobalConfiguration.java"
    This reverts commit 4c822e7f0ea2f9c341e5c438ef8664d178344c4d.

--- src/main/java/com/perros/registro_perros/config/CorsGlobalConfiguration.java
+++ src/main/java/com/perros/registro_perros/config/CorsGlobalConfiguration.java
@@ -2,27 +2,26 @@ package com.perros.registro_perros.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
 public class CorsGlobalConfiguration {
 
     @Bean
-    public WebFluxConfigurer corsConfigurer() {
+    public WebFluxConfigurer corsConfigurer(){
         return new WebFluxConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(org.springframework.web.reactive.config.CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                            "http://localhost:3000",
-                            "http://localhost:5173",
-                            "http://localhost:4173"
-                        )
-                        .allowedMethods("*")
+                        .allowedOrigins("http://localhost:3000", "http://localhost:5173", "http://localhost:4173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                         .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
+                        .exposedHeaders("Authorization")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }            
+            
         };
     }
+    
 }

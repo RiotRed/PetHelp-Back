5d87c108608754dc8cec9a4af95ba2a05877e3d0
JuanDFlores <juandiegoflores0202@gmail.com>
2025-07-31 18:57:38 -0500

    Update CorsGlobalConfiguration.java

--- src/main/java/com/perros/registro_perros/config/CorsGlobalConfiguration.java
+++ src/main/java/com/perros/registro_perros/config/CorsGlobalConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
+
 @Configuration
 public class CorsGlobalConfiguration {
 
@@ -13,13 +14,14 @@ public class CorsGlobalConfiguration {
             @Override
             public void addCorsMappings(org.springframework.web.reactive.config.CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(
+                            "http://localhost:3000",
+                            "https://pethelpfront-n4pwofitc-alessandro-rojas-projects.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }            
-            
         };
     }
-    
 }
